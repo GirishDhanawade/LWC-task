3e19fb2aa1457e19568a472105c613607f47d9bf
Girish_Dhanawade <109794360+GirishDhanawade@users.noreply.github.com>
2023-03-11 18:36:57 +0530

    Add files via upload

---
+++ objectSelector/objectSelector.js
@@ -0,0 +1,30 @@
+import { LightningElement, wire, track } from 'lwc';
+//import { refreshApex } from '@salesforce/apex';
+//import getObjectInfo from '@salesforce/apex/ObjectSelectorController.getObjectInfo';
+import getObjects from '@salesforce/apex/ObjectSelectorController.getObjects';
+export default class ObjectSelector extends LightningElement {
+    @track objectList = [];
+    @track myfeilds = [];
+    @track selectedobject;
+    @wire(getObjects)
+    wiredObjectName({ error, data }) {
+        if (data) {
+            this.objectList = Object.entries(data).map(([value, label]) => ({ value, label }));
+        } else if (error) {
+            console.error(error);
+        }
+    }
+    handleFields(event) {
+        this.selectedobject = event.target.value;
+        this.template.querySelector('c-feild-selector').getAllFields(event.target.value);
+        this.template.querySelector('c-feild-selector').hideFeilds();
+        this.template.querySelector('c-record-selector').hideData();
+
+    }
+    handlemycheckbox(event) {
+        this.myfeilds = event.detail;
+        console.log('MyFeilds' + this.myfeilds);
+        console.log('selected object' + this.selectedobject);
+        this.template.querySelector('c-record-selector').getallRecords(this.selectedobject, this.myfeilds);
+    }
+}
